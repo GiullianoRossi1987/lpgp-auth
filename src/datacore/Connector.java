da10d158e0d66e4049fff94950b6fd2b3d3183f7
GiullianoRossi1987 <giulliano.scatalon.rossi@gmail.com>
2020-05-30 22:51:38 -0300

    Started building

---
+++ src/datacore/Connector.java
@@ -0,0 +1,10 @@
+package datacore;
+
+import org.gjt.mm.mysql.Driver;
+import java.lang.Exception;
+import java.lang.ClassNotFoundException;
+import config.SocketConfig;
+
+public class Connector{
+    // todo
+}
